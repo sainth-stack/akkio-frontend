97d8b1c158a42d0724cf9cbdfd08bec9fd6a9a27
VishnuVardhanBonala-web4U <rvardhancm08@gmail.com>
2024-12-24 23:46:18 +0530

    "updated pages"

---
+++ src/pages/Pricing/Data/ListItems.js
@@ -0,0 +1,33 @@
+
+ export const FirstCardDetails = [
+    {  text: ' Chat Explore' },
+    {  text: ' Chat Data Preparation' },
+    {  text: ' Live Generative BI dashboards' },
+];
+  
+
+ export const FourthCardDetails = [
+   { text: " Chat Explore" },
+ ];
+  
+
+
+ export const SecondCardDetails = [
+    {  text: ' Whitelabel Chat Explore' },
+     {
+         text: 'Insights Reports' },
+    {  text: ' Access API for all Chat and ML features' },
+];
+  
+
+
+ export const ThirdCardDetails = [
+   { text: "Dedicated Infrastructure" },
+   { text: "Run in your data environment (Snowflake, Databricks, etc.)" },
+   { text: "Custom LLMs" },
+   { text: "Larger User Counts" },
+   { text: "Advanced White Labeling" },
+   { text: "Advanced API access" },
+   { text: "Role and client-based access controls" },
+   { text: "Enterprise support /customer success engineering" },
+ ];
---
+++ src/pages/Pricing/Data/Plans.js
@@ -0,0 +1,35 @@
+export const plans = [
+  {
+    title: "Free",
+    description: "Great for individuals starting out.",
+    price: "0",
+    features: ["Connect","Discover"],
+    buttonText: "GET STARTED",
+    buttonLink: "/login",
+  },
+
+  {
+    title: "Basic",
+    description: "Great for individuals starting out.",
+    price: "99",
+    features: [ "Connect","Discover","Chat Data Prep"],
+    buttonText: "GET STARTED",
+    buttonLink: "https://superprofile.bio/vp/676a743280809a00134c2119",
+  },
+  {
+    title: "Standard",
+    description: "Ideal for small teams and businesses.",
+    price: "1999",
+    features: ["Connect", "Discover","Chat Data Prep", "KPI"],
+    buttonText: "CONTACT US",
+    buttonLink: "/contact",
+  },
+  {
+    title: "Advanced",
+    description: "For growing businesses and enterprises.",
+    price: "2999",
+    features: ["Connect", "Discover","Chat Data Prep", "Visualize", "KPI","Reports"],
+    buttonText: "CONTACT US",
+    buttonLink: "/contact",
+  },
+];
